8ea388f9852c2d41d6250c7b6f2017e44f7d332b
Anthony C. Emmanuel <mymi14s@hotmail.com>
2024-01-24 19:53:12 +0100

    Revert "Feat: Leave Application"

--- src/views/CheckinHistory.vue
+++ src/views/CheckinHistory.vue
@@ -25,11 +25,11 @@ export default {
     },
     methods:{
       async getData(){
-        
         // Get leave data
         this.frappe.customApiCall("api/method/one_fm.api.v2.web.get_checkin_history", {
           employee: this.employee_data.name}, 'POST').then(res=>{
-            console.log(this.employee_data.employee_id)
+
+            console.log(res)
           if (res.status_code==200){
             this.checkin_history = res.data.logs;
             this.start = res.data.start;
